371a8bae396d0556fa78dc4acc4e0bf077e7c166
EmilioAK <emilio.alvarez96@gmail.com>
2020-12-10 22:26:50 +0100

    Prettier

--- script.js
+++ script.js
@@ -9,10 +9,7 @@ document.querySelector("#submitbutton").style.display = "none";
 document.querySelector(".scorebox").style.display = "none";
 document.querySelector("#timerContainer").style.display = "none";
 document.querySelector("#submitbutton").addEventListener("click", function() {
-    (this.clicked != undefined) ? this.clicked = !this.clicked : this.clicked = true;
     get_results();
-    document.querySelector("#timerContainer").style.display = "none";
-
 });
 
 const timer_text = document.getElementById("timer");
@@ -21,21 +18,34 @@ let interval_id;
 let reading_time_timer = READING_TIME;
 let answer_time_timer = ANSWER_TIME;
 
-start_button.addEventListener("click", function() {
+const presentTask = function () {
+    const updateClock = function () {
+      if (reading_time_timer > 0) {
+        timer_text.textContent = reading_time_timer;
+        reading_time_timer -= 1;
+      } else if (answer_time_timer > 0) {
+        timer_text.textContent = answer_time_timer;
+        answer_time_timer -= 1;
+      } else {
+        clearInterval(interval_id);
+      }
+    };
+  
+    const read_section_end = function () {
+      setTimeout(() => {
+        test_section_elements();
+        setTimeout(() => {
+            get_results();
+        }, READING_TIME * 1000); // Since it expects milliseconds and reading and answer_time are in seconds
+      }, ANSWER_TIME * 1000);
+    };
+  
     read_section_elements();
-    read_section_end()
-    interval_id = setInterval(() => {
-        if (reading_time_timer > 0) {
-            timer_text.textContent = reading_time_timer;
-            reading_time_timer -= 1;   
-        } else if (answer_time_timer > 0) {
-            timer_text.textContent = answer_time_timer;
-            answer_time_timer -= 1;
-        } else {
-            clearInterval(interval_id);
-        }
-    }, 1000);
-});
+    read_section_end();
+    interval_id = setInterval(updateClock, 1000);
+  }
+  
+  start_button.addEventListener("click", presentTask);
 
 
 const read_section_elements = function () {
@@ -67,24 +77,10 @@ const get_user_input = function () {
     return user_input_array;
 }
 
-const read_section_end = function () {
-    setTimeout(() => {
-        test_section_elements();
-        setTimeout(() => {
-            if (!document.querySelector("#submitbutton").clicked) {
-                alert("Sorry, you took too long");
-                document.body.innerHTML = "";
-            }
-        }, READING_TIME * 1000); // Since it expects milliseconds and reading and answer_time are in seconds
-    }, ANSWER_TIME * 1000);
-
-
-}
-
 const get_results = function () {
     document.querySelector("#submitbutton").style.display = "none";
     document.querySelector("#answer").style.display = "none";
-    document.querySelector(".scorebox").innerHTML += score_user(get_user_input());
+    document.querySelector("#score").innerHTML = score_user(get_user_input());
     document.querySelector(".scorebox").style.display = "block";
-
+    document.querySelector("#timerContainer").style.display = "none";
 }
