53e84652144ddc83d134418a6dd62fff67b60c98
EmilioAK <emilio.alvarez96@gmail.com>
2020-12-11 18:37:37 +0100

    const

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-WORD_LIST_1 = ["Word1", "Word2", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3"];
-WORD_LIST_2 = ["Blurb1", "Blurb2", "Blurb3", "Blurb4"];
+const WORD_LIST_1 = ["Word1", "Word2", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3", "Word3"];
+const WORD_LIST_2 = ["Blurb1", "Blurb2", "Blurb3", "Blurb4"];
 
 const WORDS = (window.location.hash ? WORD_LIST_2 : WORD_LIST_1);
 const READING_TIME = 120; // Both given in seconds
